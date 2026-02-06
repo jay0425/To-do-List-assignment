2094712cc5336acac78d5e97c4fccb738854c699
김재희 <rolarola0315@gooddaysoft.com>
2026-02-07 06:20:42 +0900

    chore: public svg 추가

---
+++ public/index.ts
@@ -0,0 +1,27 @@
+import SmallLogoIcon from "./small-logo.svg";
+import LargeLogoIcon from "./large-logo.svg";
+import NoImageIcon from "./img.svg";
+import ToDoIcon from "./todo.svg";
+import DoneIcon from "./done.svg";
+import ToDoEmptyIcon from "./todo-empty.svg";
+import DoneEmptyIcon from "./done-empty.svg";
+import Memo from "./memo.svg";
+import CheckIcon from "./check.svg";
+import SmallCheckIcon from "./small-check.svg";
+import XIcon from "./x.svg";
+import PencilIcon from "./pencil.svg";
+
+export {
+  SmallLogoIcon,
+  LargeLogoIcon,
+  NoImageIcon,
+  ToDoIcon,
+  DoneIcon,
+  ToDoEmptyIcon,
+  DoneEmptyIcon,
+  Memo,
+  CheckIcon,
+  SmallCheckIcon,
+  XIcon,
+  PencilIcon,
+};
