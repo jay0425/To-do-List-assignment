2419c8f048e82e00ac21345d9c0675484e1bda40
김재희 <rolarola0315@gooddaysoft.com>
2026-02-07 11:58:33 +0900

    moved: svg 파일들 이동

---
+++ svgr.d.ts
@@ -0,0 +1,11 @@
+declare module "*.svg" {
+  import React from "react";
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare module "*.svg?react" {
+  import * as React from "react";
+  const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default Component;
+}
