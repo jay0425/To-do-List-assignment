6b875caa151cdadbc1a6dc27f9766980d4571ad9
김재희 <rolarola0315@gooddaysoft.com>
2026-02-07 22:36:18 +0900

    move: public 이미지 파일 이동

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,13 +1,11 @@
-// public/icons/index.ts
-
-import SmallLogoIcon from "./small-logo.svg?react";
-import LargeLogoIcon from "./large-logo.svg?react";
+import SmallLogoIcon from "../small-logo.svg?react";
+import LargeLogoIcon from "../large-logo.svg?react";
 import NoImageIcon from "./img.svg?react";
 import ToDoIcon from "./todo.svg?react";
 import DoneIcon from "./done.svg?react";
 import ToDoEmptyIcon from "./todo-empty.svg?react";
 import DoneEmptyIcon from "./done-empty.svg?react";
-import Memo from "./memo.svg?react";
+import Memo from "../memo.svg?react";
 import CheckIcon from "./check.svg?react";
 import SmallCheckIcon from "./small-check.svg?react";
 import XIcon from "./x.svg?react";
